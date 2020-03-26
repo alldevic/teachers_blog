046e52b2fc289be07e4a42e6c162bd8cdc2c77c9
Nikolay Bely <belyynikolay@gmail.com>
2020-03-27 00:48:10 +0700

    Add initial ptvsd support

--- manage.py
+++ manage.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
-import os
+
+from os import environ as env
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.dev")
+    env.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.dev")
 
     from django.core.management import execute_from_command_line
+    from django.conf import settings
 
+    if settings.DEBUG and \
+            (env.get('RUN_MAIN') or env.get('WERKZEUG_RUN_MAIN')):
+        import ptvsd
+        ptvsd.enable_attach(address=('0.0.0.0', 5678))
+        print("Attached remote debugger")
     execute_from_command_line(sys.argv)
